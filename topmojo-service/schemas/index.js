a6343d73e3a1d0a8d2bfb3413feeefe27f4da157
Amal221200 <92168224+Amal221200@users.noreply.github.com>
2023-11-18 14:38:20 +0530

    Delete topmojo-service directory

--- topmojo-service/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import author from "./author";
-import category from "./category";
-import post from "./post";
-import info from "./info";
-
-export const schemaTypes = [category, post, author, info];
--- topmojo-service/schemas/reference/authorReference.js
+++
@@ -1,6 +0,0 @@
-export default {
-    name: 'author',
-    type: 'reference',
-    title: 'Author',
-    to: [{ type: 'author' }]
-}
